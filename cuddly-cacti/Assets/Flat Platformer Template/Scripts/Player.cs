643d3b93c4294306d4b63c70dbc7a001c97abc8f
Anders <anders.hansson@weknowit.nu>
2021-03-13 17:52:17 +0100

    Add Death handling

--- cuddly-cacti/Assets/Flat Platformer Template/Scripts/Player.cs
+++ cuddly-cacti/Assets/Flat Platformer Template/Scripts/Player.cs
@@ -5,7 +5,8 @@ using System.Collections.Generic;
 using UnityEngine.Serialization;
 
 [RequireComponent(typeof(Rigidbody2D))]
-public class Player : MonoBehaviour {
+public class Player : MonoBehaviour
+{
     public float WalkSpeed;
     public float JumpForce;
     public float JetForce;
@@ -17,7 +18,10 @@ public class Player : MonoBehaviour {
     public float JetFuel;
     public float JetFuelConsumptionModifier = 1.0f;
     public float JetPowerModifier = 1.0f;
-    public float InAirMod = 1.0f;
+    public float InAirMod = 20.0f;
+    
+    public Vector2 respawnPos = new Vector2(5.9f, -40.9f);
+    public int deathCount = 0;
 
 
 
@@ -28,8 +32,9 @@ public class Player : MonoBehaviour {
     private Vector2 _inputAxis;
     private RaycastHit2D _hit;
     private float _jetFuelMax = 1.0f;
+    
 
-	void Start ()
+    void Start()
     {
         rig = gameObject.GetComponent<Rigidbody2D>();
         _startScale = transform.localScale.x;
@@ -38,7 +43,8 @@ public class Player : MonoBehaviour {
 
     void Update()
     {
-        if (_hit = Physics2D.Linecast(new Vector2(_GroundCast.position.x, _GroundCast.position.y + 0.2f), _GroundCast.position))
+        if (_hit = Physics2D.Linecast(new Vector2(_GroundCast.position.x, _GroundCast.position.y + 0.2f),
+            _GroundCast.position))
         {
             if (!_hit.transform.CompareTag("Player"))
             {
@@ -64,7 +70,7 @@ public class Player : MonoBehaviour {
         {
             JetFuel -= Time.deltaTime * JetFuelConsumptionModifier;
         }
-        
+
         //Jets while pressing space
         //Change to microphone and change values of modifiers
         _isJet = _canJet && Input.GetKey(KeyCode.Space);
@@ -87,6 +93,7 @@ public class Player : MonoBehaviour {
             transform.localScale = new Vector3(_startScale, _startScale, 1);
             _Blade.transform.rotation = Quaternion.AngleAxis(rot, Vector3.forward);
         }
+
         if (mirror)
         {
             rot = Mathf.Atan2(-dir.y, -dir.x) * Mathf.Rad2Deg;
@@ -96,7 +103,7 @@ public class Player : MonoBehaviour {
 
         if (_inputAxis.x != 0)
         {
-            if(_canJump)
+            if (_canJump)
                 rig.velocity = new Vector2(_inputAxis.x * WalkSpeed * Time.deltaTime, rig.velocity.y);
             else
             {
@@ -110,7 +117,7 @@ public class Player : MonoBehaviour {
             }
         }
 
-        else if(_canJump)
+        else if (_canJump)
         {
             rig.velocity = new Vector2(0, rig.velocity.y);
         }
@@ -140,4 +147,22 @@ public class Player : MonoBehaviour {
     {
         Gizmos.DrawLine(transform.position, _GroundCast.position);
     }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        print(other.tag);
+        if (other.tag == "Death")
+        {
+            
+            handleDeath();
+        }
+    }
+
+    private void handleDeath()
+    {
+        rig.position = respawnPos;
+        deathCount++;
+
+    }
 }
+
