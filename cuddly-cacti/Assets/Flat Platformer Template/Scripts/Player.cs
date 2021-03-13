6aa321fab07f5d26e3a2de9b90933a8813b98358
Anders <anders.hansson@weknowit.nu>
2021-03-13 23:34:33 +0100

    fix wrong line of code

--- cuddly-cacti/Assets/Flat Platformer Template/Scripts/Player.cs
+++ cuddly-cacti/Assets/Flat Platformer Template/Scripts/Player.cs
@@ -172,7 +172,6 @@ public class Player : MonoBehaviour
                 break;
             case "Checkpoint": HandleCheckPoint(other);
                 break;
-                        handleDeath();
         }
     }
 
